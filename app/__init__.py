4390805666d443dcba2c24a313e450438fbe9efc
waimea-ldhunt <ldhunt@waimea.school.nz>
2025-06-09 11:00:40 +1200

    Made it work

--- app/__init__.py
+++ app/__init__.py
@@ -34,11 +34,11 @@ def connect_db():
 @app.get("/")
 def home():
     client = connect_db()
-    result = client.execute("SELECT * FROM things")
+    result = client.execute("SELECT * FROM shop")
 
-    print(result.rows)
+    things = result.rows
 
-    return render_template("pages/home.jinja")
+    return render_template("pages/home.jinja", things=things)
 
 
 #-----------------------------------------------------------
@@ -46,7 +46,16 @@ def home():
 #-----------------------------------------------------------
 @app.get("/thing/<int:id>")
 def show_thing(id):
-    return render_template("pages/thing.jinja")
+    client = connect_db()
+    sql = """
+        SELECT id, item, price, stock
+        FROM shop
+        WHERE id=?
+    """
+    values = [id]
+    result = client.execute(sql, values)
+    thing = result.rows[0]
+    return render_template("pages/thing.jinja",thing=thing)
 
 
 #-----------------------------------------------------------
